e38989b6cfd67253619158f054aa90e183dc4d97
techops <fanxiao1987821@gmail.com>
2014-10-15 11:03:49 +0800

    just commit

--- easymock/src/test/java/com/erinfan/easymock/UserServiceTest.java
+++ easymock/src/test/java/com/erinfan/easymock/UserServiceTest.java
@@ -1,7 +1,11 @@
 package com.erinfan.easymock;
 
+import com.sun.deploy.util.StringUtils;
 import org.junit.Before;
 import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static junit.framework.Assert.assertTrue;
 import static org.easymock.EasyMock.*;
@@ -32,4 +36,36 @@ public class UserServiceTest {
         assertTrue(userService.registerUser(user));
         verify(userDao);
     }
+
+    @Test
+    public void should_TEst() throws Exception {
+        List<String> string1 = createStrings("a", "b");
+        List<String> string2 = createStrings("a", null);
+        List<String> string3 = createStrings(null, "b");
+        List<String> string4 = createStrings(null, null);
+
+        System.out.println(StringUtils.join(string2, ", "));
+        System.out.println(StringUtils.join(string2, ", "));
+        System.out.println(StringUtils.join(string2, ", "));
+        System.out.println(StringUtils.join(string2, ", "));
+
+
+    }
+
+    @Test
+    public void should_replace_two_int_values() throws Exception {
+        int first = 1;
+        int second = 2;
+
+        ActionErrors errors = new ActionErrors();
+    }
+
+    private List<String> createStrings(String a, String b) {
+        List<String> first = new ArrayList<>();
+        first.add(a);
+        first.add(b);
+        return first;
+    }
 }
+
+SELECT C.NAME AS CUSTOMER_NAME, P.NAME AS PRODUCT_NAME, P.PRICE, PR.QUANTITY, P.PRICE * PR.QUANTITY AS TOTLE, PR.CREATE_TIME , sum(P.PRICE * PR.QUANTITY) FROM CUSTOMERS AS C LEFT JOIN PURCHASES AS PR ON PR.CUSTOMER_ID=C.CUSTOMER_NO LEFT JOIN PRODUCTS AS P ON P.ID = PR.PRODUCT_ID group by MONTH(PR.CREATE_TIME);
